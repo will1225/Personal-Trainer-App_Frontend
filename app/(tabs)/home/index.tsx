4b2ada426d620752d65f95e40bcea76778c591db
Keith Carlos <keithcarlos34@gmail.com>
2024-09-15 11:15:14 -0400

    dev config

--- app/(tabs)/home/index.tsx
+++ app/(tabs)/home/index.tsx
@@ -1,18 +1,31 @@
-import { StyleSheet } from 'react-native';
+import { Button, Linking, StyleSheet } from 'react-native';
 
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
 import { Link } from 'expo-router';
+import { AccessToken, LoginButton, LoginResult } from 'react-native-fbsdk-next';
 
 export default function TabOneScreen() {
+
+  const onLoginFinished = async (err: Record<string, unknown>, result: LoginResult) => {
+    const data = await AccessToken.getCurrentAccessToken();
+    const accessToken = data?.accessToken.toString();
+    fetch(`https://graph.facebook.com/me?fields=id,name,picture,email&access_token=${accessToken}`).then(res => res.json()).then(userData => {
+      console.log(userData)
+    })
+
+  }
+
   return (
     <View style={styles.container}>
       <Link push href="/home/hello" style={{color: "white", fontWeight: "bold", marginVertical: 10}} >
         Hello!!!!
       </Link>
+      <LoginButton onLoginFinished={onLoginFinished} onLogoutFinished={() => console.log("User logged out")} />
       <Text style={styles.title}>Tab One</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="app/(tabs)/index.tsx" />
+
     </View>
   );
 }
