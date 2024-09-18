0da788b9e89bcef0d0c5fc1c4b703829d0e2bde7
will1225 <ixus_tse@hotmail.com>
2024-09-18 12:26:27 -0400

    Add third party login (facebook) component - Removed dob and gender fields from sign-up - Edited password regex in sign-up and sign-in

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -4,6 +4,7 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 import { CustomButton, FormField } from "../../components";
 import { View, Text, Alert, Image } from "react-native";
+import { LoginButton } from "react-native-fbsdk-next";
 import * as user from "../../app/controllers/user";
 import BackButton from "../../components/BackButton";
 
@@ -23,7 +24,7 @@ const SignIn = () => {
 
   // Regex for email and password validation
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  const passwordRegex = /^(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{8,}$/; // At least 1 uppercase and 1 number, minimum 8 chars
+  const passwordRegex = /^(?=.*[A-Z])(?=.*\d)[A-Za-z\d!@#$%^&*()_+[\]{};':"\\|,.<>\/?~-]{8,}$/; // At least 1 uppercase and 1 number, minimum 8 chars
 
   // Form submission handling
   const submit = async () => {
@@ -53,7 +54,6 @@ const SignIn = () => {
     setSubmitting(true);
 
     try {
-      // TODO: Confirm home page route with Faisal
       // API call
       const result = await user.loginUser(email, password);
       if (result.status) {
@@ -73,7 +73,7 @@ const SignIn = () => {
         enableOnAndroid={true}
       >
         <BackButton />
-        <View className="w-full flex justify-center items-center px-4 my-6">
+        <View className="w-full flex justify-center items-center px-4 my-6" >
           <View className="w-full flex justify-center items-center px-4 mb-6">
             <Text className="text-3xl font-bold text-center">
               Personal Trainer App
@@ -114,10 +114,22 @@ const SignIn = () => {
 
           <Link
             href={"/forgotPassword" as Href<string>}
-            className="text-lg font-psemibold text-secondary mt-4"
+            className="text-lg font-psemibold text-secondary mt-4 mb-8"
           >
             Forgot Password
           </Link>
+          <View className="w-full flex justify-center items-center">
+            <Text className="text-base  text-center font-psemibold w-full mb-3">
+              Or continue with providers:
+            </Text>
+            <View style={{ transform: [{ scaleX: 1.2 }, { scaleY: 1.2 }] }}>
+              <LoginButton
+                onLoginFinished={user.facebookLogin}
+                onLogoutFinished={() => console.log("User logged out")}
+                permissions={["email", "public_profile"]}
+              />
+            </View>
+          </View>
         </View>
       </KeyboardAwareScrollView>
     </SafeAreaView>
