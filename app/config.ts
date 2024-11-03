f8a3011f568067943b747d20ba1554c32d393c68
Arina Kolodeznikova <arinak1017@gmail.com>
2024-11-03 10:25:18 -0500

    added config.ts to gitignore

--- app/config.ts
+++
@@ -1,5 +0,0 @@
-// Production/Testing flag
-let production = false; // Set to true in Production
-let endpoint = production ? `https://7u45qve0xl.execute-api.ca-central-1.amazonaws.com/dev` : `http://10.10.6.150:8080`; // Replace with your own ip4 address for test
-
-export { production, endpoint };
