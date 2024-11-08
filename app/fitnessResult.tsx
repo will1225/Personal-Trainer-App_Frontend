5bdaf684c0b485461ec8866d243deff8025a7b4b
Arina Kolodeznikova <arinak1017@gmail.com>
2024-11-07 22:34:52 -0500

    imported progress summary component, added fetching of the related data

--- app/fitnessResult.tsx
+++ app/fitnessResult.tsx
@@ -3,46 +3,59 @@ import React, { useEffect, useState } from "react";
 import BackButton from "@/components/BackButton";
 import { Href, Link, router, useLocalSearchParams } from "expo-router";
 import CustomButton from "@/components/CustomButton";
+import ProgressSummary from "@/components/ProgressSummary";
 import { StatusBar } from "expo-status-bar";
 import * as fitnessUtil from "./controllers/fitnessResult";
+import { getProgressResults } from "./controllers/progress";
 import { useQueryClient } from "react-query";
 
 // Fitness Result screen
 const fitnessResult = () => {
   const queryClient = useQueryClient();
   const image = require("../assets/images/neonDumbell.png");  
-  const { measurementId } = useLocalSearchParams(); // measurementId passing from the previous screen
+  const { measurementId, isProgress } = useLocalSearchParams(); // measurementId, progress passing from the previous screen
   if (!measurementId) throw "Measurement ID is missing";
-  
-  // Hardcoded test
-  // const measurementId = '2';
 
+  
   // State variables
   const [bodyFat, setBodyFat] = useState<number | null>(null);
   const [muscleMass, setMuscleMass] = useState<number | null>(null);
   const [classification, setClassification] = useState<string | null>(null);
   const [ffmiClassification, setFFMIClassification] = useState<string | null>(null);
   const [ranges, setRanges] = useState<any[]>([]);
+  const [progressSummary, setProgressSummary] = useState<any | null>(null);
+  const [loading, setLoading] = useState(true);
 
   // Get and set fitness data
   useEffect(() => {
-    const getBodyFatPercentage = async () => {
-      const result = await fitnessUtil.fetchFitnessResult(measurementId as string);
-      setBodyFat(result.bodyFatPercent);
-      setMuscleMass(result.muscleMass);
-      setClassification(result.classification); 
-      setFFMIClassification(result.ffmiClassification); 
-      setRanges(result.ranges.classifications || []);
+    const getFitnessData = async () => {
+      setLoading(true);
+
+      const fitnessResult = await fitnessUtil.fetchFitnessResult(measurementId as string);
+      setBodyFat(fitnessResult.bodyFatPercent);
+      setMuscleMass(fitnessResult.muscleMass);
+      setClassification(fitnessResult.classification); 
+      setFFMIClassification(fitnessResult.ffmiClassification); 
+      setRanges(fitnessResult.ranges.classifications || []);
 
       // Save intensity and level after fetching fitness result
-      const updateResult = await fitnessUtil.saveIntensityAndLevel(result.classification, result.ffmiClassification);
+      const updateResult = await fitnessUtil.saveIntensityAndLevel(fitnessResult.classification, fitnessResult.ffmiClassification);
       if (updateResult.status) {
         queryClient.invalidateQueries({
           queryKey: ['profile']
         });
       }
+
+      // Get the progress summary info if needed to display progress results
+      if (isProgress) {
+        const progressResult = await getProgressResults();
+        setProgressSummary(progressResult);
+      }
+      setLoading(false);
     };
-    getBodyFatPercentage();
+
+
+    getFitnessData();
   }, [measurementId]);
 
   // Highlight the corresponding row based on body fat %
@@ -63,140 +76,152 @@ const fitnessResult = () => {
             Your Current Fitness Level
           </Text>
 
-          <Image
-            source={image}
-            resizeMode="contain"
-            className="w-[200px] h-[50px] justify-center mb-6"
-          />
+          {/* Render a loading state while the data is being fetched */}
+          {loading ? (
+            <Text className="text-lg text-center mt-4">Loading...</Text>
+          ) : ( 
+            <>
+              {/* Render progress summary if needed, else render the image */}
+              {progressSummary ? (
+                <ProgressSummary data={progressSummary} />
+              ) : !isProgress ? (
+                <Image
+                  source={image}
+                  resizeMode="contain"
+                  className="w-[200px] h-[50px] justify-center mb-6"
+                />
+              ) : null}
 
-          <View className="flex flex-col items-center mb-6 mt-2">
-            <View className="flex flex-row justify-center">
-              <Text className="text-xl w-48 text-right">
-                Body Fat %:
-              </Text>
-              <Text className="text-xl ml-2">
-                {bodyFat !== null ? `${bodyFat} %` : "Loading..."}
-              </Text>
-            </View>
-            <View className="flex flex-row justify-center mt-2">
-              <Text className="text-xl w-48 text-right">
-                Lean Body Mass:
-              </Text>
-              <Text className="text-xl ml-2">
-                {muscleMass !== null ? `${muscleMass} kg` : "Loading..."}
-              </Text>
-            </View>
-            {classification && (
-              <View className="flex flex-row justify-center mt-2">
-                <Text className="text-xl w-48 text-right">
-                  Body Fat Class:
-                </Text>
-                <Text className="text-xl ml-2">
-                  {classification}
+              <View className="flex flex-col items-center mb-6 mt-2">
+                <View className="flex flex-row justify-center">
+                  <Text className="text-xl w-48 text-right">
+                    Body Fat %:
+                  </Text>
+                  <Text className="text-xl ml-2">
+                    {bodyFat !== null ? `${bodyFat} %` : "Loading..."}
+                  </Text>
+                </View>
+                <View className="flex flex-row justify-center mt-2">
+                  <Text className="text-xl w-48 text-right">
+                    Lean Body Mass:
+                  </Text>
+                  <Text className="text-xl ml-2">
+                    {muscleMass !== null ? `${muscleMass} kg` : "Loading..."}
+                  </Text>
+                </View>
+                {classification && (
+                  <View className="flex flex-row justify-center mt-2">
+                    <Text className="text-xl w-48 text-right">
+                      Body Fat Class:
+                    </Text>
+                    <Text className="text-xl ml-2">
+                      {classification}
+                    </Text>
+                  </View>
+                )}
+                {ffmiClassification && (
+                  <View className="flex flex-row justify-center mt-2">
+                    <Text className="text-xl w-48 text-right">
+                      FFMI Class:
+                    </Text>
+                    <Text className="text-xl ml-2">
+                      {ffmiClassification}
+                    </Text>
+                  </View>
+                )}
+                <Text className="text-sm text-left px-1 mt-6">
+                  ** Body Fat Class reflects your body fat for your age, 
+                  while FFMI Class indicates your muscle mass relative to your height.
                 </Text>
               </View>
-            )}
-            {ffmiClassification && (
-              <View className="flex flex-row justify-center mt-2">
-                <Text className="text-xl w-48 text-right">
-                  FFMI Class:
+
+              {/* Body Fat Percentage Table */}
+              <View className="w-full mb-8">
+                <Text
+                  className="text-2xl font-bold text-center mb-0"
+                  style={{ backgroundColor: "#fbbf24", height: 40, lineHeight: 40 }}
+                >
+                  Body Fat Chart
                 </Text>
-                <Text className="text-xl ml-2">
-                  {ffmiClassification}
-                </Text>
+                <View
+                  style={{
+                    flexDirection: "row",
+                    justifyContent: "space-between",
+                    alignItems: "center",
+                    backgroundColor: "#fcd34d",
+                    height: 30,
+                  }}
+                >
+                  <Text
+                    className="font-bold"
+                    style={{
+                      flex: 1,
+                      textAlign: "center",
+                      fontSize: 16,
+                      backgroundColor: "#fcd34d",
+                    }}
+                  >
+                    Classification
+                  </Text>
+                  <Text
+                    className="font-bold"
+                    style={{
+                      flex: 1,
+                      textAlign: "center",
+                      fontSize: 16,
+                      backgroundColor: "#fcd34d",
+                    }}
+                  >
+                    Men
+                  </Text>
+                  <Text
+                    className="font-bold"
+                    style={{
+                      flex: 1,
+                      textAlign: "center",
+                      fontSize: 16,
+                      backgroundColor: "#fcd34d",
+                    }}
+                  >
+                    Women
+                  </Text>
+                </View>
+
+                {ranges.map((item, index) => (
+                  <View
+                    key={index}
+                    style={{
+                      flexDirection: "row",
+                      justifyContent: "space-between",
+                      alignItems: "center",
+                      height: 32,
+                      backgroundColor: isHighlighted(item) ? "#4ade80" : "#e5e5e5",
+                      borderBottomWidth: 1,
+                      borderBottomColor: "#ddd",
+                    }}
+                  >
+                    <Text style={{ flex: 1, textAlign: "center" }}>{item.classification}</Text>
+                    <Text style={{ flex: 1, textAlign: "center" }}>{item.men}</Text>
+                    <Text style={{ flex: 1, textAlign: "center" }}>{item.women}</Text>
+                  </View>
+                ))}
               </View>
-            )}
-            <Text className="text-sm text-left px-1 mt-6">
-              ** Body Fat Class reflects your body fat for your age, 
-              while FFMI Class indicates your muscle mass relative to your height.
-            </Text>
-          </View>
 
-          {/* Body Fat Percentage Table */}
-          <View className="w-full mb-8">
-            <Text
-              className="text-2xl font-bold text-center mb-0"
-              style={{ backgroundColor: "#fbbf24", height: 40, lineHeight: 40 }}
-            >
-              Body Fat Chart
-            </Text>
-            <View
-              style={{
-                flexDirection: "row",
-                justifyContent: "space-between",
-                alignItems: "center",
-                backgroundColor: "#fcd34d",
-                height: 30,
-              }}
-            >
-              <Text
-                className="font-bold"
-                style={{
-                  flex: 1,
-                  textAlign: "center",
-                  fontSize: 16,
-                  backgroundColor: "#fcd34d",
-                }}
-              >
-                Classification
-              </Text>
-              <Text
-                className="font-bold"
-                style={{
-                  flex: 1,
-                  textAlign: "center",
-                  fontSize: 16,
-                  backgroundColor: "#fcd34d",
-                }}
-              >
-                Men
-              </Text>
-              <Text
-                className="font-bold"
-                style={{
-                  flex: 1,
-                  textAlign: "center",
-                  fontSize: 16,
-                  backgroundColor: "#fcd34d",
-                }}
-              >
-                Women
-              </Text>
-            </View>
+              <Text className="text-xl text-left mb-8">
+                Ready to get in shape?
+              </Text>          
 
-            {ranges.map((item, index) => (
-              <View
-                key={index}
-                style={{
-                  flexDirection: "row",
-                  justifyContent: "space-between",
-                  alignItems: "center",
-                  height: 32,
-                  backgroundColor: isHighlighted(item) ? "#4ade80" : "#e5e5e5",
-                  borderBottomWidth: 1,
-                  borderBottomColor: "#ddd",
-                }}
-              >
-                <Text style={{ flex: 1, textAlign: "center" }}>{item.classification}</Text>
-                <Text style={{ flex: 1, textAlign: "center" }}>{item.men}</Text>
-                <Text style={{ flex: 1, textAlign: "center" }}>{item.women}</Text>
-              </View>
-            ))}
-          </View>
-
-          <Text className="text-xl text-left mb-8">
-            Ready to get in shape?
-          </Text>          
-
-          <CustomButton
-            title="Get Personalized Routine"
-            handlePress={() => router.push("/generateRoutine" as Href<string>)}
-            containerStyles="w-[230px]"
-          />
-          
-          <Link href={"/(tabs)/home" as Href<string>} className="text-lg font-psemibold text-grey mt-4 mb-8">
-            I'll do it later.
-          </Link>
+              <CustomButton
+                title="Get Personalized Routine"
+                handlePress={() => router.push("/generateRoutine" as Href<string>)}
+                containerStyles="w-[230px]"
+              />
+              
+              <Link href={"/(tabs)/home" as Href<string>} className="text-lg font-psemibold text-grey mt-4 mb-8">
+                I'll do it later.
+              </Link>
+            </>
+          )}
         </View>
       </ScrollView>
       <StatusBar backgroundColor="#161622" style="light" />
