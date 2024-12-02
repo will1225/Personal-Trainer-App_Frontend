084bed26d875dbee29e3b741cefb0011df51a1c4
Keith Carlos <108279070+Kratos3434@users.noreply.github.com>
2024-12-02 10:36:14 -0500

    Update config.ts

--- app/config.ts
+++ app/config.ts
@@ -1,6 +1,6 @@
 // Production/Testing flag
 let production = true; // Set to true in Production
 let endpoint = production ? `https://7u45qve0xl.execute-api.ca-central-1.amazonaws.com/dev` : `http://10.0.0.44:8080`; // Replace with your own ip4 address for test
-const isDemoMode = true;
+const isDemoMode = false;
 
 export { production, endpoint, isDemoMode };
